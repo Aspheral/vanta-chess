b5e818d6f7825b17d1572253323126ab6119d897
Aspheral <aspheralmusic@gmail.com>
2026-08-19 11:45:56 -0500

    Use tactical V2 search in the worker

--- src/engine/worker.js
+++ src/engine/worker.js
@@ -1,5 +1,5 @@
 import { Position, moveToUci } from '../chess/position.js';
-import { SearchEngine } from './search.js';
+import { SearchEngine } from './search-v2.js';
 
 let activeSearchId=0;
 let engine=new SearchEngine();
