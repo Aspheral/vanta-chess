1063172286ded6cd9b11d586c9bdd2be846451a3
Aspheral <aspheralmusic@gmail.com>
2026-08-29 03:47:05 -0500

    Add recent Chess.com regression fixtures

---
+++ tests/fixtures/recent-games.js
@@ -0,0 +1,27 @@
+export const DEV_EXPERT_GAME = `[Date "2026.08.29"]
+[White "itzvanta"]
+[Black "DevTheExpertBack"]
+[Result "0-1"]
+
+1. Nf3 Nf6 2. Nc3 d5 3. Ne5 d4 4. Na4 Qd6 5. f4 b5 6. c3 bxa4 7. Qxa4+ Bd7 8. Qb3 e6 9. Qc4 dxc3 10. Qxc3 Nc6 11. Kd1 Rd8 12. h4 Nd5 13. Qg3 Nxf4 14. Nd3 Nxd3 15. Qxd3 Qxd3 16. exd3 Nd4 17. h5 e5 18. Rb1 Bg4+ 19. Ke1 Bc5 20. Kf2 Nf5+ 21. d4 Rxd4 22. Bb5+ Ke7 23. Kf1 Rf4+ 24. Ke1 Bf2+ 25. Kf1 Ng3# 0-1`;
+
+export const DANH_GAME = `[Date "2026.08.29"]
+[White "itzvanta"]
+[Black "danhngunhunglanhlung"]
+[Result "0-1"]
+
+1. Nc3 d5 2. Nf3 Nc6 3. a4 Bg4 4. b3 e6 5. Ba3 Bxa3 6. Rxa3 Qd6 7. Nb5 Qe7 8. Qa1 a6 9. Qxg7 axb5 10. Ra2 Qf6 11. Qxg4 bxa4 12. bxa4 Nh6 13. Qg5 Qxg5 14. Nxg5 Nb4 15. Rb2 Rxa4 16. f3 O-O 17. c3 Nc6 18. Rxb7 Ra1+ 19. Kf2 Rc8 20. Kg1 d4 21. cxd4 Nxd4 22. e3 Ndf5 23. Rb2 Kg7 24. e4 Nd4 25. e5 Kh8 26. Rb7 Rg8 27. h4 Nhf5 28. Nxf7+ Kg7 29. Rxc7 Ng3 30. Ng5+ Kg6 31. h5+ Kxg5 32. f4+ Kxf4 33. Rh4+ Kxe5 34. Rc5+ Kd6 35. Rxd4+ Kxc5 36. Rc4+ Kd5 37. d3 Rxf1+ 38. Kh2 Rh1# 0-1`;
+
+export const BLUE_SWORD_GAME = `[Date "2026.08.19"]
+[White "itzvanta"]
+[Black "theblueswordele"]
+[Result "0-1"]
+
+1. Nc3 e5 2. Nf3 Nc6 3. Nd5 Nf6 4. Ne3 e4 5. Nh4 Bc5 6. Nhf5 O-O 7. c3 Bxe3 8. Nxe3 d5 9. Qb3 Rb8 10. Qa3 Be6 11. Rb1 a5 12. b3 Re8 13. Bb2 h6 14. Qc5 Qd7 15. Ba3 b6 16. Qb5 Ne5 17. Qxd7 Bxd7 18. h4 b5 19. Bc5 Be6 20. Ba7 Rb7 21. Bc5 Nfd7 22. Bd4 Nc6 23. Rc1 Nxd4 24. cxd4 Rc8 25. g3 c5 26. Bg2 c4 27. b4 axb4 28. Rc2 Ra8 29. O-O Rba7 30. Ra1 b3 31. Rb2 Rxa2 32. Rbxa2 Rxa2 33. Rxa2 bxa2 34. Nc2 Nb6 35. e3 Na8 36. f3 exf3 37. Bxf3 Nc7 38. e4 dxe4 39. Bxe4 Na6 40. d3 cxd3 41. Bxd3 Nb4 42. Be4 Nxc2 43. Bxc2 a1=Q+ 44. Kh2 Qb2 45. h5 Qxc2+ 46. Kg1 Bh3 47. Kh1 Qg2# 0-1`;
+
+export const JEMBO_GAME = `[Date "2026.08.18"]
+[White "Jembojem"]
+[Black "itzvanta"]
+[Result "1-0"]
+
+1. e4 Nf6 2. Nf3 Nxe4 3. Nc3 Nxc3 4. dxc3 Nc6 5. Bc4 a5 6. Ng5 Ne5 7. Qe2 Nxc4 8. Qxc4 d5 9. Qd3 Bg4 10. h3 Bh5 11. Bf4 h6 12. Nf3 Bxf3 13. gxf3 c5 14. Rg1 d4 15. cxd4 Qxd4 16. Qxd4 cxd4 17. O-O-O g5 18. Be5 f6 19. Bxd4 Rc8 20. Rge1 Bg7 21. c3 Rc4 22. b3 Rc6 23. Kc2 b5 24. Re2 a4 25. Rde1 e5 26. Be3 Rd6 27. Bc5 axb3+ 28. axb3 Rd5 29. b4 Kf7 30. Rd2 Rxd2+ 31. Kxd2 Rd8+ 32. Ke3 Kg6 33. Re2 f5 34. Rd2 Bf6 35. Rd6 Rxd6 36. Bxd6 e4 37. fxe4 fxe4 38. c4 bxc4 39. b5 h5 40. b6 Bd8 41. b7 Bb6+ 42. Kxe4 Ba7 43. b8=Q Bxb8 44. Bxb8 h4 45. Be5 Kf7 46. f4 gxf4 47. Kxf4 Ke7 48. Kg4 Ke6 49. Ba1 Kd6 50. Kxh4 Kc5 51. Kg5 Kc6 52. h4 Kc5 53. h5 Kc6 54. h6 Kc5 55. h7 Kc6 56. h8=Q Kc5 57. Kf5 Kb5 58. Ke6 Ka5 59. Kd5 Ka4 60. Qb8 Ka3 61. Kxc4 Ka4 62. Qb4# 1-0`;
